108e4c7a7de729dce5038f6cd8958667c7de0692
MerlinHdz <merlin.hndz@gmail.com>
2024-08-16 18:55:31 -0500

    updating files

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-// npm - global command, comes with node
-// npm --version
+const EventEmitter = require('events');
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const customEmitter = new EventEmitter();
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> (mac)
+customEmitter.on('response', (name, id)=>{
+    console.log(`data received ${name} with id:${id}`);
+});
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+customEmitter.on('response', ()=>{
+    console.log(`some other logic here`);
+});
 
-const _ = require('lodash');
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// Order of this matters. If we placed the emit before the on, it would not work.
+customEmitter.emit('response', 'John', 34);
